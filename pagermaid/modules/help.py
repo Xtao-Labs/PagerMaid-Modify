f61a44eca687fb81edba070d8b04b4f6e65da55d
Xtao_dada <xtao@xtaolink.cn>
2021-04-11 01:06:34 +0800

    add lang command

--- pagermaid/modules/help.py
+++ pagermaid/modules/help.py
@@ -1,8 +1,9 @@
 """ The help module. """
 
+from os import listdir
 from pagermaid import help_messages
 from pagermaid.utils import lang
-from pagermaid.listener import listener
+from pagermaid.listener import listener, config
 
 
 @listener(is_plugin=False, outgoing=True, command="help",
@@ -21,4 +22,25 @@ async def help(context):
             result += "`" + str(command)
             result += "`, "
         await context.edit(result[:-2] + f"\n**{lang('help_send')} \"-help <{lang('command')}>\" {lang('help_see')}** "
-                                         f"[{lang('源代码')}](https://t.me/PagerMaid_Modify)")
+                                         f"[{lang('help_source')}](https://t.me/PagerMaid_Modify)")
+
+
+@listener(is_plugin=False, outgoing=True, command="lang",
+          description="切换语言。")
+async def lang_change(context):
+    to_lang = context.arguments
+    from_lang = config["application_language"]
+    dir, ldir = listdir('languages'), []
+    for i in dir:
+        if not i.find('yml') == -1:
+            ldir.append(i[:-4])
+    with open('config.yml') as f:
+        file = f.read()
+    if to_lang in ldir:
+        file = file.replace(f'application_language: "{from_lang}"', f'application_language: "{to_lang}"')
+        with open('config.yml', 'w') as f:
+            f.write(file)
+        await context.edit(f'语言已切换到 {to_lang} ，正在重新启动。')
+        await context.client.disconnect()
+    else:
+        await context.edit(f'当前语言是 {config["application_language"]}\n\n所有可用的语言有：{"，".join(ldir)}')
