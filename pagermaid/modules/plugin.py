b97053f15cd981f4c5fd4824c8c50ba9f7a84ac7
Pentacene <kbT26meV@outlook.com>
2021-04-12 15:42:21 +0800

    plugin

--- pagermaid/modules/plugin.py
+++ pagermaid/modules/plugin.py
@@ -38,7 +38,7 @@ def update_version(file_path, plugin_content, plugin_name, version):
 
 @listener(is_plugin=False, outgoing=True, command="apt", diagnostics=False,
           description=lang('apt_des'),
-          parameters="")
+          parameters=lang('apt_parameters'))
 async def plugin(context):
     if len(context.parameter) == 0:
         await context.edit(lang('arg_error'))
@@ -60,8 +60,8 @@ async def plugin(context):
                     pass
                 return
             move_plugin(file_path)
-            await context.edit(f"{lang('apt_plugin')} {path.basename(file_path)[:-3]} {lang('apt_installed')}，{lang'apt_reboot'}")
-            await log(f"{lang('apt_success')} {path.basename(file_path)[:-3]}.")
+            await context.edit(f"{lang('apt_plugin')} {path.basename(file_path)[:-3]} {lang('apt_installed')},{lang('apt_reboot')}")
+            await log(f"{lang('apt_install_success')} {path.basename(file_path)[:-3]}.")
             await context.client.disconnect()
         elif len(context.parameter) >= 2:
             await context.edit(lang('apt_processing'))
@@ -109,13 +109,13 @@ async def plugin(context):
                             move_plugin(file_path)
                             success_list.append(path.basename(file_path)[:-3])
                 if not flag:
-                    now_message += f"{apt_not_found} {plugin_name} 。\n"
+                    now_message += f"{lang('apt_not_found')} {plugin_name} 。\n"
                     failed_list.append(plugin_name)
             message = ""
             if len(success_list) > 0:
-                message += lang('apt_success') + " : %s\n" % ", ".join(success_list)
+                message += lang('apt_install_success') + " : %s\n" % ", ".join(success_list)
             if len(failed_list) > 0:
-                message += lang('apt_failed') + " %s\n" % ", ".join(failed_list)
+                message += lang('apt_install_failed') + " %s\n" % ", ".join(failed_list)
             if len(noneed_list) > 0:
                 message += lang('apt_no_update') + " %s\n" % ", ".join(noneed_list)
             await log(message)
@@ -136,8 +136,8 @@ async def plugin(context):
                 version_json[context.parameter[1]] = '0.0'
                 with open(f"{plugin_directory}version.json", 'w') as f:
                     json.dump(version_json, f)
-                await context.edit(f"成功删除插件 {context.parameter[1]}, PagerMaid-Modify 正在重新启动。")
-                await log(f"删除插件 {context.parameter[1]}.")
+                await context.edit(f"{lang('apt_remove_success')} {context.parameter[1]}, {lang('apt_reboot')} ")
+                await log(f"{lang('apt_remove')} {context.parameter[1]}.")
                 await context.client.disconnect()
             elif exists(f"{plugin_directory}{context.parameter[1]}.py.disabled"):
                 remove(f"{plugin_directory}{context.parameter[1]}.py.disabled")
@@ -146,12 +146,12 @@ async def plugin(context):
                 version_json[context.parameter[1]] = '0.0'
                 with open(f"{plugin_directory}version.json", 'w') as f:
                     json.dump(version_json, f)
-                await context.edit(f"已删除的插件 {context.parameter[1]}.")
-                await log(f"已删除的插件 {context.parameter[1]}.")
+                await context.edit(f"{lang('apt_removed_plugins')} {context.parameter[1]}.")
+                await log(f"{lang('apt_removed_plugins')} {context.parameter[1]}.")
             elif "/" in context.parameter[1]:
                 await context.edit(lang('arg_error'))
             else:
-                await context.edit("出错了呜呜呜 ~ 指定的插件不存在。")
+                await context.edit(lang('apt_not_exist'))
         else:
             await context.edit(lang('arg_error'))
     elif context.parameter[0] == "status":
@@ -178,15 +178,15 @@ async def plugin(context):
                 disabled_plugins_string += f"{target_plugin}, "
             disabled_plugins_string = disabled_plugins_string[:-2]
             if len(active_plugins) == 0:
-                active_plugins_string = "`没有运行中的插件。`"
+                active_plugins_string = f"`{lang('apt_no_running_plugins')}`"
             if len(inactive_plugins) == 0:
-                inactive_plugins_string = "`没有加载失败的插件。`"
+                inactive_plugins_string = f"`{lang('apt_no_load_falied_plugins')}`"
             if len(disabled_plugins) == 0:
-                disabled_plugins_string = "`没有关闭的插件`"
-            output = f"**插件列表**\n" \
-                     f"运行中: {active_plugins_string}\n" \
-                     f"已关闭: {disabled_plugins_string}\n" \
-                     f"加载失败: {inactive_plugins_string}"
+                disabled_plugins_string = f"`{lang('apt_no_disabled_plugins')}`"
+            output = f"**{lang('apt_plugin_list')}**\n" \
+                     f"{lang('apt_plugin_running')}: {active_plugins_string}\n" \
+                     f"{lang('apt_plugin_disabled')}: {disabled_plugins_string}\n" \
+                     f"{lang('apt_plugin_failed')}: {inactive_plugins_string}"
             await context.edit(output)
         else:
             await context.edit(lang('arg_error'))
@@ -195,11 +195,11 @@ async def plugin(context):
             if exists(f"{plugin_directory}{context.parameter[1]}.py.disabled"):
                 rename(f"{plugin_directory}{context.parameter[1]}.py.disabled",
                        f"{plugin_directory}{context.parameter[1]}.py")
-                await context.edit(f"插件 {context.parameter[1]} 已启用，PagerMaid-Modify 正在重新启动。")
-                await log(f"已启用 {context.parameter[1]}.")
+                await context.edit(f"{lang('apt_plugin')} {context.parameter[1]} {lang('apt_enable')},{lang('apt_reboot')}")
+                await log(f"{lang('apt_enable')} {context.parameter[1]}.")
                 await context.client.disconnect()
             else:
-                await context.edit("出错了呜呜呜 ~ 指定的插件不存在。")
+                await context.edit(lang('apt_not_exist'))
         else:
             await context.edit(lang('arg_error'))
     elif context.parameter[0] == "disable":
@@ -207,11 +207,11 @@ async def plugin(context):
             if exists(f"{plugin_directory}{context.parameter[1]}.py") is True:
                 rename(f"{plugin_directory}{context.parameter[1]}.py",
                        f"{plugin_directory}{context.parameter[1]}.py.disabled")
-                await context.edit(f"插件 {context.parameter[1]} 已被禁用，PagerMaid-Modify 正在重新启动。")
-                await log(f"已关闭插件 {context.parameter[1]}.")
+                await context.edit(f"{lang('apt_plugin')} {context.parameter[1]} {lang('apt_disable')},{lang('apt_reboot')}")
+                await log(f"{lang('apt_disable')} {context.parameter[1]}.")
                 await context.client.disconnect()
             else:
-                await context.edit("出错了呜呜呜 ~ 指定的插件不存在。")
+                await context.edit(lang('apt_not_exist'))
         else:
             await context.edit(lang('arg_error'))
     elif context.parameter[0] == "upload":
@@ -225,22 +225,22 @@ async def plugin(context):
             elif exists(f"{plugin_directory}{file_name}.disabled"):
                 copyfile(f"{plugin_directory}{file_name}.disabled", file_name)
             if exists(file_name):
-                await context.edit("上传插件中 . . .")
+                await context.edit(lang('apt_uploading'))
                 await upload_attachment(file_name,
                                         context.chat_id, reply_id,
                                         caption=f"PagerMaid-Modify {context.parameter[1]} plugin.")
                 remove(file_name)
                 await context.delete()
             else:
-                await context.edit("出错了呜呜呜 ~ 指定的插件不存在。")
+                await context.edit(lang('apt_not_exist'))
         else:
             await context.edit(lang('arg_error'))
     elif context.parameter[0] == "update":
-        unneed_update = "无需更新："
-        need_update = "\n已更新："
+        unneed_update = lang('apt_no_update')
+        need_update = f"\n{lang('apt_updated')}:"
         need_update_list = []
         if not exists(f"{plugin_directory}version.json"):
-            await context.edit("安装一个仓库内插件再试试？")
+            await context.edit(lang('apt_why_not_install_a_plugin'))
             return
         with open(f"{plugin_directory}version.json", 'r', encoding="utf-8") as f:
             version_json = json.load(f)
@@ -252,23 +252,23 @@ async def plugin(context):
             for i in plugin_online:
                 if key == i['name']:
                     if (float(i['version']) - float(value)) <= 0:
-                        unneed_update += "\n`" + key + "`：Ver  " + value
+                        unneed_update += "\n`" + key + "`:Ver  " + value
                     else:
                         need_update_list.extend([key])
-                        need_update += "\n`" + key + "`：Ver  " + value + " --> Ver  " + i['version']
+                        need_update += "\n`" + key + "`:Ver  " + value + " --> Ver  " + i['version']
                     continue
-        if unneed_update == "无需更新：":
+        if unneed_update == f"{lang('apt_no_update')}:":
             unneed_update = ''
-        if need_update == "\n已更新：":
+        if need_update == f"\n{lang('apt_updated')}:":
             need_update = ''
         if unneed_update == '' and need_update == '':
-            await context.edit("不如去安装一些插件？")
+            await context.edit(lang('apt_why_not_install_a_plugin'))
         else:
             if len(need_update_list) == 0:
-                await context.edit('正在读取云端插件列表...完成\n正在读取本地插件版本信息...完成\n**没有需要更新的插件。**')
+                await context.edit(lang('apt_loading_from_online_but_nothing_need_to_update'))
             else:
                 print(6)
-                await context.edit('正在读取云端插件列表...完成\n正在读取本地插件版本信息...完成\n正在更新插件...')
+                await context.edit(lang('apt_loading_from_online_and_updating'))
                 plugin_directory = f"{working_dir}/plugins/"
                 for i in need_update_list:
                     file_path = i + ".py"
@@ -284,11 +284,11 @@ async def plugin(context):
                     with open(f"{plugin_directory}version.json", 'w') as f:
                         json.dump(version_json, f)
                     move_plugin(file_path)
-                await context.edit('正在读取云端插件列表...完成\n正在读取本地插件版本信息...完成\n' + need_update)
+                await context.edit(lang('apt_reading_list') + need_update)
                 await context.client.disconnect()
     elif context.parameter[0] == "search":
         if len(context.parameter) == 1:
-            await context.edit("没插件名我怎么搜索？")
+            await context.edit(lang('apt_search_no_name'))
         elif len(context.parameter) == 2:
             search_result = []
             plugin_name = context.parameter[1]
@@ -299,14 +299,14 @@ async def plugin(context):
                 if search(plugin_name, i['name'], I):
                     search_result.extend(['`' + i['name'] + '` / `' + i['version'] + '`\n  ' + i['des-short']])
             if len(search_result) == 0:
-                await context.edit("未在插件仓库中搜索到相关插件。")
+                await context.edit(lang('apt_search_not_found'))
             else:
-                await context.edit('以下是插件仓库的搜索结果：\n\n' + '\n\n'.join(search_result))
+                await context.edit(f"{lang('apt_search_result_hint')}:\n\n" + '\n\n'.join(search_result))
         else:
             await context.edit(lang('arg_error'))
     elif context.parameter[0] == "show":
         if len(context.parameter) == 1:
-            await context.edit("没插件名我怎么显示？")
+            await context.edit(lang('apt_search_no_name'))
         elif len(context.parameter) == 2:
             search_result = ''
             plugin_name = context.parameter[1]
@@ -316,16 +316,16 @@ async def plugin(context):
             for i in plugin_online:
                 if plugin_name == i['name']:
                     if i['supported']:
-                        search_support = '仍在周期中'
+                        search_support = lang('apt_search_supporting')
                     else:
-                        search_support = '已弃疗'
-                    search_result = '插件名：`' + i['name'] + '`\n版本：`Ver  ' + i['version'] + '`\n分类：`' + i[
-                        'section'] + '`\n作者：`' + \
-                                    i['maintainer'] + '`\n大小：`' + i['size'] + '`\n支持周期：' + search_support + '\n说明：' + i[
+                        search_support = lang('apt_search_not_supporting')
+                    search_result = lang('apt_plugin_name')+ ':`' + i['name'] + '`\n' + lang('apt_plugin_ver')+ ':`Ver  ' + i['version'] + '`\n' + lang('apt_plugin_section')+ ':`' + i[
+                        'section'] + '`\n' + lang('apt_plugin_maintainer')+ ':`' + \
+                                    i['maintainer'] + '`\n' + lang('apt_plugin_size')+ ':`' + i['size'] + '`\n' + lang('apt_plugin_support')+ ':' + search_support + '\n' + lang('apt_plugin_des_short')+ ':' + i[
                                         'des-short'] + '\n\n' + i['des']
                     break
             if search_result == '':
-                await context.edit("未在插件仓库中搜索到相关插件。")
+                await context.edit(lang('apt_search_not_found'))
             else:
                 await context.edit(search_result)
     else:
